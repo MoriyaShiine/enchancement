7262b936a175fa310a04679f39a82295d204fe0d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-03-26 18:22:44 -0500

    frostbite no longer works with fire aspect

--- src/main/java/moriyashiine/enchancement/common/enchantment/FrostbiteEnchantment.java
+++ src/main/java/moriyashiine/enchancement/common/enchantment/FrostbiteEnchantment.java
@@ -2,6 +2,7 @@ package moriyashiine.enchancement.common.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -9,6 +10,11 @@ import net.minecraft.entity.LivingEntity;
 public class FrostbiteEnchantment extends Enchantment {
 	public FrostbiteEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot... slotTypes) {
 		super(weight, type, slotTypes);
+	}
+
+	@Override
+	protected boolean canAccept(Enchantment other) {
+		return super.canAccept(other) && other != Enchantments.FIRE_ASPECT;
 	}
 
 	@Override
