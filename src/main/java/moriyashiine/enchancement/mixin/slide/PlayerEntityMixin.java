68b067909a0b6c4b34617b8a1ff8c96ca1c5d1fb
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-03-28 19:19:48 -0500

    slide no longer clips at ledge

--- src/main/java/moriyashiine/enchancement/mixin/slide/PlayerEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/slide/PlayerEntityMixin.java
@@ -5,7 +5,9 @@ import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
@@ -15,5 +17,12 @@ public class PlayerEntityMixin {
 			return EntityPose.SWIMMING;
 		}
 		return value;
+	}
+
+	@Inject(method = "clipAtLedge", at = @At("HEAD"), cancellable = true)
+	private void enchancement$slide(CallbackInfoReturnable<Boolean> cir) {
+		if (ModComponents.SLIDE.get(this).shouldSlide()) {
+			cir.setReturnValue(false);
+		}
 	}
 }
