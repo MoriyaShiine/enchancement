9935035ea5191fe42aab0750d0428d7ae7e2c1a7
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-04-14 14:20:17 -0500

    refactor

--- src/main/java/moriyashiine/enchancement/mixin/berserk/client/ItemRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/berserk/client/ItemRendererMixin.java
@@ -20,16 +20,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
+	@Inject(method = "renderGuiItemModel", at = @At("HEAD"))
+	private void enchancement$berserk(ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
+		EnchancementClientUtil.berserkColor = EnchancementClientUtil.getBerserkColor(MinecraftClient.getInstance().player, stack);
+	}
+
 	@Inject(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;III)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/render/item/ItemRenderer;getModel(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;I)Lnet/minecraft/client/render/model/BakedModel;"))
 	private void enchancement$berserk(LivingEntity entity, ItemStack item, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, int seed, CallbackInfo ci) {
 		if (entity != null) {
 			EnchancementClientUtil.berserkColor = EnchancementClientUtil.getBerserkColor(entity, item);
 		}
-	}
-
-	@Inject(method = "renderGuiItemModel", at = @At("HEAD"))
-	private void enchancement$berserk(ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
-		EnchancementClientUtil.berserkColor = EnchancementClientUtil.getBerserkColor(MinecraftClient.getInstance().player, stack);
 	}
 
 	@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At("TAIL"))
