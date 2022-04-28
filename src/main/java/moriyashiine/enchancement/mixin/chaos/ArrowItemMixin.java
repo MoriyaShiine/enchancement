68c4eaf6cdfffb11f4398ddc928b79f053adba2b
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-04-27 19:10:10 -0500

    chaos now changes based on sneaking state

--- src/main/java/moriyashiine/enchancement/mixin/chaos/ArrowItemMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/chaos/ArrowItemMixin.java
@@ -3,6 +3,9 @@ package moriyashiine.enchancement.mixin.chaos;
 import moriyashiine.enchancement.common.registry.ModEnchantments;
 import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ArrowItem;
@@ -16,13 +19,25 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collections;
+
 @Mixin(ArrowItem.class)
 public class ArrowItemMixin {
 	@Inject(method = "createArrow", at = @At("HEAD"), cancellable = true)
 	private void enchancement$chaos(World world, ItemStack stack, LivingEntity shooter, CallbackInfoReturnable<PersistentProjectileEntity> cir) {
 		if (stack.getItem() == Items.ARROW && EnchancementUtil.hasEnchantment(ModEnchantments.CHAOS, shooter)) {
 			ArrowEntity arrow = new ArrowEntity(world, shooter);
-			arrow.initFromStack(PotionUtil.setPotion(new ItemStack(Items.TIPPED_ARROW), Registry.POTION.get(shooter.getRandom().nextInt(Registry.POTION.size()))));
+			StatusEffect effect = null;
+			if (shooter.isSneaking()) {
+				while (effect == null || effect.getCategory() != StatusEffectCategory.BENEFICIAL) {
+					effect = Registry.STATUS_EFFECT.get(shooter.getRandom().nextInt(Registry.STATUS_EFFECT.size()));
+				}
+			} else {
+				while (effect == null || effect.getCategory() != StatusEffectCategory.HARMFUL) {
+					effect = Registry.STATUS_EFFECT.get(shooter.getRandom().nextInt(Registry.STATUS_EFFECT.size()));
+				}
+			}
+			arrow.initFromStack(PotionUtil.setCustomPotionEffects(new ItemStack(Items.TIPPED_ARROW), Collections.singleton(new StatusEffectInstance(effect, effect.isInstant() ? 1 : 200, shooter.getRandom().nextInt(3) == 0 ? 1 : 0))));
 			arrow.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
 			cir.setReturnValue(arrow);
 		}
