798bb7b229058ff0334eaf6250855cab39baf137
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-04-30 20:23:06 -0500

    fix lightning rod channeling

---
+++ src/main/java/moriyashiine/enchancement/mixin/vanillachanges/LightningRodBlockMixin.java
@@ -0,0 +1,15 @@
+package moriyashiine.enchancement.mixin.vanillachanges;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import moriyashiine.enchancement.common.Enchancement;
+import net.minecraft.block.LightningRodBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LightningRodBlock.class)
+public class LightningRodBlockMixin {
+	@ModifyExpressionValue(method = "onProjectileHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isThundering()Z"))
+	private boolean enchancement$channelingWorksWhenNotThundering(boolean value) {
+		return value || Enchancement.getConfig().channelingWorksWhenNotThundering;
+	}
+}
