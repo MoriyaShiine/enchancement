69e0d49ca81181e0211d2cc9906bd969bd66e775
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-01 13:03:08 -0500

    oops

--- src/main/java/moriyashiine/enchancement/mixin/torch/CrossbowItemMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/torch/CrossbowItemMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(CrossbowItem.class)
 public class CrossbowItemMixin {
 	@ModifyVariable(method = "createArrow", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ArrowItem;createArrow(Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/LivingEntity;)Lnet/minecraft/entity/projectile/PersistentProjectileEntity;"))
-	private static PersistentProjectileEntity enchancemenet$torch(PersistentProjectileEntity value, World world, LivingEntity entity, ItemStack crossbow, ItemStack arrow) {
+	private static PersistentProjectileEntity enchancement$torch(PersistentProjectileEntity value, World world, LivingEntity entity, ItemStack crossbow, ItemStack arrow) {
 		if (arrow.isOf(Items.TORCH)) {
 			return new TorchEntity(world, entity);
 		}
