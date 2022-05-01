82bcd525811b144e02a62511e2c0d36d58c85915
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-04-30 22:08:46 -0500

    phasing fix?

--- src/main/java/moriyashiine/enchancement/mixin/phasing/PersistentProjectileEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/phasing/PersistentProjectileEntityMixin.java
@@ -6,10 +6,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -36,20 +38,35 @@ public abstract class PersistentProjectileEntityMixin extends Entity {
 			if (phasingComponent.shouldPhase()) {
 				BlockState state = world.getBlockState(blockHitResult.getBlockPos());
 				state.onProjectileHit(world, state, blockHitResult, PersistentProjectileEntity.class.cast(this));
-				if (!world.isClient) {
-					Vec3d target = getPos().add(getVelocity());
-					EntityHitResult entityHitResult = getEntityCollision(getPos(), target.add(getVelocity()));
-					if (entityHitResult != null) {
-						onEntityHit(entityHitResult);
+				double distance = 0;
+				Vec3d start = getPos(), end = start.add(getVelocity().multiply(1 / 8F).normalize());
+				while (distance < 4) {
+					BlockHitResult hitResult = world.raycast(new RaycastContext(start, end, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, this));
+					if (hitResult.getType() == HitResult.Type.MISS) {
+						break;
 					}
-					setNoGravity(false);
-					((ServerWorld) world).spawnParticles(ParticleTypes.REVERSE_PORTAL, blockHitResult.getPos().getX(), blockHitResult.getPos().getY(), blockHitResult.getPos().getZ(), 6, getWidth() / 2, getHeight() / 2, getWidth() / 2, 0);
-					((ServerWorld) world).spawnParticles(ParticleTypes.REVERSE_PORTAL, target.getX(), target.getY(), target.getZ(), 6, getWidth() / 2, getHeight() / 2, getWidth() / 2, 0);
-					teleport(target.getX(), target.getY(), target.getZ());
-					phasingComponent.setShouldPhase(false);
-					phasingComponent.sync();
+					distance = getPos().distanceTo(hitResult.getPos());
+					start = end;
+					end = start.add(getVelocity().multiply(1 / 8F).normalize());
 				}
-				ci.cancel();
+				if (distance <= 3.1) {
+					if (!world.isClient) {
+						Vec3d target = getPos().add(getVelocity());
+						EntityHitResult entityHitResult = getEntityCollision(getPos(), target.add(getVelocity()));
+						if (entityHitResult != null) {
+							onEntityHit(entityHitResult);
+						}
+						setNoGravity(false);
+						teleport(end.getX(), end.getY(), end.getZ());
+					} else {
+						for (int i = 0; i < 6; i++) {
+							world.addParticle(ParticleTypes.REVERSE_PORTAL, blockHitResult.getPos().getX() + MathHelper.nextDouble(random, -getWidth() / 2, getWidth() / 2), blockHitResult.getPos().getY() + MathHelper.nextDouble(random, -getHeight() / 2, getHeight() / 2), blockHitResult.getPos().getZ() + MathHelper.nextDouble(random, -getWidth() / 2, getWidth() / 2), 0, 0, 0);
+							world.addParticle(ParticleTypes.REVERSE_PORTAL, end.getX() + MathHelper.nextDouble(random, -getWidth() / 2, getWidth() / 2), end.getY() + MathHelper.nextDouble(random, -getHeight() / 2, getHeight() / 2), end.getZ() + MathHelper.nextDouble(random, -getWidth() / 2, getWidth() / 2), 0, 0, 0);
+						}
+					}
+					phasingComponent.setShouldPhase(false);
+					ci.cancel();
+				}
 			}
 		});
 	}
