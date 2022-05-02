8fc5723dcd76f2fc1c5d847fea9e08de16f55aec
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-02 12:41:16 -0500

    fix thing

--- src/main/java/moriyashiine/enchancement/mixin/impact/client/PlayerEntityRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/impact/client/PlayerEntityRendererMixin.java
@@ -4,21 +4,26 @@ import moriyashiine.enchancement.common.registry.ModEntityComponents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Environment(EnvType.CLIENT)
 @Mixin(PlayerEntityRenderer.class)
-public class PlayerEntityRendererMixin {
-	@Inject(method = "setModelPose", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void enchancement$impact(AbstractClientPlayerEntity player, CallbackInfo ci, PlayerEntityModel<AbstractClientPlayerEntity> model) {
+public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+	public PlayerEntityRendererMixin(EntityRendererFactory.Context ctx, PlayerEntityModel<AbstractClientPlayerEntity> model, float shadowRadius) {
+		super(ctx, model, shadowRadius);
+	}
+
+	@Inject(method = "setModelPose", at = @At("TAIL"))
+	private void enchancement$impact(AbstractClientPlayerEntity player, CallbackInfo ci) {
 		if (ModEntityComponents.IMPACT.get(player).shouldForceSneak()) {
-			model.sneaking = true;
+			getModel().sneaking = true;
 		}
 	}
 }
--- src/main/java/moriyashiine/enchancement/mixin/slide/client/PlayerEntityRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/slide/client/PlayerEntityRendererMixin.java
@@ -4,6 +4,8 @@ import moriyashiine.enchancement.common.registry.ModEntityComponents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.util.math.Vec3d;
@@ -12,11 +14,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Environment(EnvType.CLIENT)
 @Mixin(PlayerEntityRenderer.class)
-public class PlayerEntityRendererMixin {
+public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+	public PlayerEntityRendererMixin(EntityRendererFactory.Context ctx, PlayerEntityModel<AbstractClientPlayerEntity> model, float shadowRadius) {
+		super(ctx, model, shadowRadius);
+	}
+
 	@Inject(method = "getPositionOffset(Lnet/minecraft/client/network/AbstractClientPlayerEntity;F)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
 	private void enchancement$slide(AbstractClientPlayerEntity player, float f, CallbackInfoReturnable<Vec3d> cir) {
 		if (ModEntityComponents.SLIDE.get(player).shouldSlide()) {
@@ -24,10 +29,10 @@ public class PlayerEntityRendererMixin {
 		}
 	}
 
-	@Inject(method = "setModelPose", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void enchancement$slide(AbstractClientPlayerEntity player, CallbackInfo ci, PlayerEntityModel<AbstractClientPlayerEntity> model) {
+	@Inject(method = "setModelPose", at = @At("TAIL"))
+	private void enchancement$slide(AbstractClientPlayerEntity player, CallbackInfo ci) {
 		if (ModEntityComponents.SLIDE.get(player).shouldSlide()) {
-			model.sneaking = false;
+			getModel().sneaking = false;
 		}
 	}
 }
