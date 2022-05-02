d8c2ec792bffdb0c186c61f0bb028835feb37770
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-02 11:20:06 -0500

    fix exclusive enchantments

---
+++ src/main/java/moriyashiine/enchancement/mixin/util/EnchantmentHelperMixin.java
@@ -0,0 +1,24 @@
+package moriyashiine.enchancement.mixin.util;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(EnchantmentHelper.class)
+public class EnchantmentHelperMixin {
+	@Inject(method = "getPossibleEntries", at = @At("RETURN"))
+	private static void enchancement$fixExclusiveEnchantments(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+		List<EnchantmentLevelEntry> entries = cir.getReturnValue();
+		for (int i = entries.size() - 1; i >= 0; i--) {
+			if (!entries.get(i).enchantment.isAcceptableItem(stack)) {
+				entries.remove(i);
+			}
+		}
+	}
+}
