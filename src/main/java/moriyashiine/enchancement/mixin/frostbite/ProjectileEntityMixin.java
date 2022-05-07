562886afe6cda30b2cf2b7429e40eb2c86fd6c39
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-07 17:04:51 -0500

    fix frozen entities being immune to projectiles

---
+++ src/main/java/moriyashiine/enchancement/mixin/frostbite/ProjectileEntityMixin.java
@@ -0,0 +1,21 @@
+package moriyashiine.enchancement.mixin.frostbite;
+
+import moriyashiine.enchancement.common.registry.ModEntityComponents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ProjectileEntity.class)
+public class ProjectileEntityMixin {
+	@Inject(method = "canHit", at = @At("HEAD"), cancellable = true)
+	private void enchancement$frostbite(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+		ModEntityComponents.FROZEN.maybeGet(entity).ifPresent(frozenComponent -> {
+			if (frozenComponent.isFrozen()) {
+				cir.setReturnValue(true);
+			}
+		});
+	}
+}
