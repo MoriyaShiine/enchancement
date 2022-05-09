295b7caf9f6771ee981780e10b2f09ce6941cd7d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-09 12:41:45 -0500

    add headers

--- src/main/java/moriyashiine/enchancement/mixin/brimstone/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/brimstone/LivingEntityMixin.java
@@ -1,3 +1,7 @@
+/*
+ * All Rights Reserved (c) 2022 MoriyaShiine
+ */
+
 package moriyashiine.enchancement.mixin.brimstone;
 
 import moriyashiine.enchancement.common.entity.projectile.BrimstoneEntity;
--- src/main/java/moriyashiine/enchancement/mixin/disarm/PiglinBrainAccessor.java
+++ src/main/java/moriyashiine/enchancement/mixin/disarm/PiglinBrainAccessor.java
@@ -1,3 +1,7 @@
+/*
+ * All Rights Reserved (c) 2022 MoriyaShiine
+ */
+
 package moriyashiine.enchancement.mixin.disarm;
 
 import net.minecraft.entity.LivingEntity;
