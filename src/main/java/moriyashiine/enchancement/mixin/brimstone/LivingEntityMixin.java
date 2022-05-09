99bb7f36e825b6445c4205b6abd3b04a55e38f05
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-09 08:55:41 -0500

    brimstone now pierces shields

---
+++ src/main/java/moriyashiine/enchancement/mixin/brimstone/LivingEntityMixin.java
@@ -0,0 +1,19 @@
+package moriyashiine.enchancement.mixin.brimstone;
+
+import moriyashiine.enchancement.common.entity.projectile.BrimstoneEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+	@Inject(method = "blockedByShield", at = @At("HEAD"), cancellable = true)
+	private void enchancement$brimstone(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+		if (source.getSource() instanceof BrimstoneEntity) {
+			cir.setReturnValue(false);
+		}
+	}
+}
