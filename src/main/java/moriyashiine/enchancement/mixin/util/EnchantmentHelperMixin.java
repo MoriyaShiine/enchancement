ecd74b6e52d9ec95385bcd88ffd429c9a9e8217c
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-10 09:14:00 -0500

    fix books not being enchantable with overhaul enchanting table turned off

--- src/main/java/moriyashiine/enchancement/mixin/util/EnchantmentHelperMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/util/EnchantmentHelperMixin.java
@@ -7,6 +7,7 @@ package moriyashiine.enchancement.mixin.util;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,10 +19,12 @@ import java.util.List;
 public class EnchantmentHelperMixin {
 	@Inject(method = "getPossibleEntries", at = @At("RETURN"))
 	private static void enchancement$fixExclusiveEnchantments(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
-		List<EnchantmentLevelEntry> entries = cir.getReturnValue();
-		for (int i = entries.size() - 1; i >= 0; i--) {
-			if (!entries.get(i).enchantment.isAcceptableItem(stack)) {
-				entries.remove(i);
+		if (!stack.isOf(Items.BOOK)) {
+			List<EnchantmentLevelEntry> entries = cir.getReturnValue();
+			for (int i = entries.size() - 1; i >= 0; i--) {
+				if (!entries.get(i).enchantment.isAcceptableItem(stack)) {
+					entries.remove(i);
+				}
 			}
 		}
 	}
