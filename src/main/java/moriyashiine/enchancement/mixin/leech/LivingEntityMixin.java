1a2ec0994b3771ff00b4b8b1a1c03ac765b87f1e
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-05-10 13:04:04 -0500

    fix leech bypassing shields

--- src/main/java/moriyashiine/enchancement/mixin/leech/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/leech/LivingEntityMixin.java
@@ -12,19 +12,23 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+	@Shadow
+	public abstract boolean blockedByShield(DamageSource source);
+
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 
-	@Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/damage/DamageSource;isFire()Z", ordinal = 0))
+	@Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"))
 	private void enchancement$leech(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-		if (source.getSource() instanceof TridentEntity trident) {
+		if (source.getSource() instanceof TridentEntity trident && !blockedByShield(source)) {
 			ModEntityComponents.LEECH.maybeGet(trident).ifPresent(leechComponent -> {
 				if (leechComponent.hasLeech() && leechComponent.getStuckEntity() == null) {
 					LivingEntity living = LivingEntity.class.cast(this);
