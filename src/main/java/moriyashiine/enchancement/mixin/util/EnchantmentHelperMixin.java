09150e6bcf2a7927e36c30bf99f3f0342af29b29
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-06-13 14:32:01 -0500

    fix #8

--- src/main/java/moriyashiine/enchancement/mixin/util/EnchantmentHelperMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/util/EnchantmentHelperMixin.java
@@ -4,10 +4,12 @@
 
 package moriyashiine.enchancement.mixin.util;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,12 +20,21 @@ import java.util.List;
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
 	@Inject(method = "getPossibleEntries", at = @At("RETURN"))
-	private static void enchancement$fixExclusiveEnchantments(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+	private static void enchancement$fixExclusiveEnchantmentsAndForceAtLeastOneEnchantment(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
 		if (!stack.isOf(Items.BOOK)) {
 			List<EnchantmentLevelEntry> entries = cir.getReturnValue();
 			for (int i = entries.size() - 1; i >= 0; i--) {
 				if (!entries.get(i).enchantment.isAcceptableItem(stack)) {
 					entries.remove(i);
+				}
+			}
+			if (entries.isEmpty()) {
+				for (int i = 0; i < Registry.ENCHANTMENT.size(); i++) {
+					Enchantment enchantment = Registry.ENCHANTMENT.get(i);
+					if (enchantment != null && enchantment.isAcceptableItem(stack)) {
+						entries.add(new EnchantmentLevelEntry(enchantment, 1));
+						return;
+					}
 				}
 			}
 		}
