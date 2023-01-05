5084901c7ed9d16088e8780a3fc57e6e9d227148
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-01-04 20:02:19 -0600

    fix #75

---
+++ src/main/java/moriyashiine/enchancement/mixin/vanillachanges/unbreakingchanges/SetDamageLootFunctionMixin.java
@@ -0,0 +1,24 @@
+/*
+ * All Rights Reserved (c) 2022 MoriyaShiine
+ */
+
+package moriyashiine.enchancement.mixin.vanillachanges.unbreakingchanges;
+
+import moriyashiine.enchancement.common.util.EnchancementUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.function.SetDamageLootFunction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SetDamageLootFunction.class)
+public class SetDamageLootFunctionMixin {
+	@Inject(method = "process", at = @At("HEAD"), cancellable = true)
+	private void enchancement$unbreakingChanges(ItemStack stack, LootContext context, CallbackInfoReturnable<ItemStack> cir) {
+		if (EnchancementUtil.shouldBeUnbreakable(stack)) {
+			cir.setReturnValue(stack);
+		}
+	}
+}
