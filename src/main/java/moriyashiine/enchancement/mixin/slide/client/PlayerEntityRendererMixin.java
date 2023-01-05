21542ad05a6faf76cdf075b7adb7ed1bd369739f
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-01-04 20:53:35 -0600

    fix #51

--- src/main/java/moriyashiine/enchancement/mixin/slide/client/PlayerEntityRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/slide/client/PlayerEntityRendererMixin.java
@@ -12,25 +12,16 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
 	public PlayerEntityRendererMixin(EntityRendererFactory.Context ctx, PlayerEntityModel<AbstractClientPlayerEntity> model, float shadowRadius) {
 		super(ctx, model, shadowRadius);
-	}
-
-	@Inject(method = "getPositionOffset(Lnet/minecraft/client/network/AbstractClientPlayerEntity;F)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
-	private void enchancement$slide(AbstractClientPlayerEntity player, float f, CallbackInfoReturnable<Vec3d> cir) {
-		if (ModEntityComponents.SLIDE.get(player).shouldSlide()) {
-			cir.setReturnValue(Vec3d.ZERO);
-		}
 	}
 
 	@Inject(method = "setModelPose", at = @At("TAIL"))
