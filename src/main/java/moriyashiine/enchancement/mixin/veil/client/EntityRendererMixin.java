639e3ba177c0f84c41298723b15d97b542d0665d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-01-20 17:14:19 -0600

    perception can now see name tags of veiled entities

--- src/main/java/moriyashiine/enchancement/mixin/veil/client/EntityRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/veil/client/EntityRendererMixin.java
@@ -25,7 +25,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EntityRendererMixin<T extends Entity> {
 	@Inject(method = "renderLabelIfPresent", at = @At("HEAD"), cancellable = true)
 	private void enchancement$veil(T entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		if (MinecraftClient.getInstance().cameraEntity instanceof LivingEntity cameraEntity && entity instanceof LivingEntity living && !living.isGlowing() && EnchancementUtil.hasEnchantment(ModEnchantments.VEIL, living) && !cameraEntity.canSee(living)) {
+		if (MinecraftClient.getInstance().cameraEntity instanceof LivingEntity cameraEntity && entity instanceof LivingEntity living && !living.isGlowing() && EnchancementUtil.hasEnchantment(ModEnchantments.VEIL, living) && !EnchancementUtil.hasEnchantment(ModEnchantments.PERCEPTION, cameraEntity) && !cameraEntity.canSee(living)) {
 			ci.cancel();
 		}
 	}
