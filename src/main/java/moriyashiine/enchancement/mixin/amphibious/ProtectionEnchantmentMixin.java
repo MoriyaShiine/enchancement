7fc66bda3b8d2018382b1e989e983c866b2df2d1
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-01-21 14:07:52 -0600

    amphibious now halves burn time

---
+++ src/main/java/moriyashiine/enchancement/mixin/amphibious/ProtectionEnchantmentMixin.java
@@ -0,0 +1,24 @@
+/*
+ * All Rights Reserved (c) 2022 MoriyaShiine
+ */
+
+package moriyashiine.enchancement.mixin.amphibious;
+
+import moriyashiine.enchancement.common.registry.ModEnchantments;
+import moriyashiine.enchancement.common.util.EnchancementUtil;
+import net.minecraft.enchantment.ProtectionEnchantment;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ProtectionEnchantment.class)
+public class ProtectionEnchantmentMixin {
+	@ModifyVariable(method = "transformFireDuration", at = @At("HEAD"), argsOnly = true)
+	private static int enchancement$amphibious(int value, LivingEntity living) {
+		if (value > 0 && EnchancementUtil.hasEnchantment(ModEnchantments.AMPHIBIOUS, living)) {
+			return Math.max(1, value / 2);
+		}
+		return value;
+	}
+}
