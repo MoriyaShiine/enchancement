767f42056e08af3768b0c933c54c30f6a3e82ab8
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-02-12 14:21:57 -0600

    fix slide sometimes taking collision damage

--- src/main/java/moriyashiine/enchancement/mixin/slide/EntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/slide/EntityMixin.java
@@ -3,9 +3,12 @@ package moriyashiine.enchancement.mixin.slide;
 import moriyashiine.enchancement.common.component.entity.SlideComponent;
 import moriyashiine.enchancement.common.registry.ModEntityComponents;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
@@ -20,5 +23,15 @@ public class EntityMixin {
 			}
 		}
 		return value;
+	}
+
+	@Inject(method = "isInvulnerableTo", at = @At("RETURN"), cancellable = true)
+	private void enchancement$slide(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+		if (!cir.getReturnValueZ() && damageSource == DamageSource.IN_WALL) {
+			SlideComponent slideComponent = ModEntityComponents.SLIDE.getNullable(this);
+			if (slideComponent != null && slideComponent.isSliding()) {
+				cir.setReturnValue(true);
+			}
+		}
 	}
 }
