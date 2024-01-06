a6f8491620f9d78a2c5bf1c4b5cad7774cdbc3b2
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-01-05 20:28:03 -0600

    fix buoy lag

--- src/main/java/moriyashiine/enchancement/mixin/buoy/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/buoy/LivingEntityMixin.java
@@ -4,34 +4,24 @@
 
 package moriyashiine.enchancement.mixin.buoy;
 
-import moriyashiine.enchancement.common.init.ModEnchantments;
-import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-	@Shadow
-	public abstract ItemStack getEquippedStack(EquipmentSlot var1);
-
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 
+	@SuppressWarnings("CancellableInjectionUsage")
 	@Inject(method = "canWalkOnFluid", at = @At("HEAD"), cancellable = true)
 	protected void enchancement$buoy(FluidState state, CallbackInfoReturnable<Boolean> cir) {
-		if (!isSneaking() && EnchancementUtil.hasEnchantment(ModEnchantments.BUOY, getEquippedStack(EquipmentSlot.FEET))) {
-			cir.setReturnValue(true);
-		}
 	}
 }
