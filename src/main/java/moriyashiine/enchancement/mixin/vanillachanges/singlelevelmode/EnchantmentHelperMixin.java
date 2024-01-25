a0f5918eed12f9061154ccf1c95f4f5febf28859
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-01-24 23:53:27 -0600

    weak tools are given lesser enchantments in single level mode

--- src/main/java/moriyashiine/enchancement/mixin/vanillachanges/singlelevelmode/EnchantmentHelperMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/vanillachanges/singlelevelmode/EnchantmentHelperMixin.java
@@ -8,16 +8,20 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import moriyashiine.enchancement.common.ModConfig;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Arrays;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
@@ -38,7 +42,7 @@ public class EnchantmentHelperMixin {
 			NbtList nbtList = stack.getEnchantments();
 			for (int i = 0; i < nbtList.size(); i++) {
 				NbtCompound nbtCompound = nbtList.getCompound(i);
-				Registries.ENCHANTMENT.getOrEmpty(EnchantmentHelper.getIdFromNbt(nbtCompound)).ifPresent(enchantment -> consumer.accept(enchantment, enchantment.getMaxLevel()));
+				Registries.ENCHANTMENT.getOrEmpty(EnchantmentHelper.getIdFromNbt(nbtCompound)).ifPresent(enchantment -> consumer.accept(enchantment, getModifiedMaxLevel(stack, enchantment.getMaxLevel())));
 			}
 			ci.cancel();
 		}
@@ -47,7 +51,7 @@ public class EnchantmentHelperMixin {
 	@Inject(method = "getLevel", at = @At("RETURN"), cancellable = true)
 	private static void enchancement$singleLevelMode(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
 		if (cir.getReturnValueI() > 0 && ModConfig.singleLevelMode) {
-			cir.setReturnValue(enchantment.getMaxLevel());
+			cir.setReturnValue(getModifiedMaxLevel(stack, enchantment.getMaxLevel()));
 		}
 	}
 
@@ -57,5 +61,27 @@ public class EnchantmentHelperMixin {
 			return 1;
 		}
 		return value;
+	}
+
+	@Unique
+	private static int getModifiedMaxLevel(ItemStack stack, int maxLevel) {
+		boolean weak = false;
+		if (stack.getItem() instanceof ArmorItem armorItem) {
+			ArmorMaterial material = armorItem.getMaterial();
+			weak = material == ArmorMaterials.LEATHER || material == ArmorMaterials.IRON || material == ArmorMaterials.CHAIN;
+			if (!weak && Arrays.stream(ArmorMaterials.values()).noneMatch(armorMaterial -> armorMaterial == material)) {
+				weak = material.getEnchantability() <= ArmorMaterials.IRON.getEnchantability();
+			}
+		} else if (stack.getItem() instanceof ToolItem toolItem) {
+			ToolMaterial material = toolItem.getMaterial();
+			weak = material == ToolMaterials.WOOD || material == ToolMaterials.STONE || material == ToolMaterials.IRON;
+			if (!weak && Arrays.stream(ToolMaterials.values()).noneMatch(toolMaterial -> toolMaterial == material)) {
+				weak = material.getEnchantability() <= ToolMaterials.IRON.getEnchantability();
+			}
+		}
+		if (weak) {
+			return MathHelper.ceil(maxLevel / 2F);
+		}
+		return maxLevel;
 	}
 }
