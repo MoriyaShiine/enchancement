6634fb7fe523a3e9b77764834e5e7696c82ce483
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-01-25 11:21:06 -0600

    amphibious fire reduction rebalance

--- src/main/java/moriyashiine/enchancement/mixin/amphibious/ProtectionEnchantmentMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/amphibious/ProtectionEnchantmentMixin.java
@@ -5,9 +5,10 @@
 package moriyashiine.enchancement.mixin.amphibious;
 
 import moriyashiine.enchancement.common.init.ModEnchantments;
-import moriyashiine.enchancement.common.util.EnchancementUtil;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.ProtectionEnchantment;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -16,8 +17,11 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class ProtectionEnchantmentMixin {
 	@ModifyVariable(method = "transformFireDuration", at = @At("HEAD"), argsOnly = true)
 	private static int enchancement$amphibious(int value, LivingEntity living) {
-		if (value > 0 && EnchancementUtil.hasEnchantment(ModEnchantments.AMPHIBIOUS, living)) {
-			return Math.max(1, value / 2);
+		if (value > 0) {
+			int level = EnchantmentHelper.getEquipmentLevel(ModEnchantments.AMPHIBIOUS, living);
+			if (level > 0) {
+				return MathHelper.ceil(value * (level == 1 ? 0.75F : 0.5F));
+			}
 		}
 		return value;
 	}
