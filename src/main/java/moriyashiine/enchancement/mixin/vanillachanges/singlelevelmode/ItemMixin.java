ef800f2fd2803b008ea4d8b9a2538952cb8d3d08
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-04-18 23:12:08 -0500

    fix #177

--- src/main/java/moriyashiine/enchancement/mixin/vanillachanges/singlelevelmode/ItemMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/vanillachanges/singlelevelmode/ItemMixin.java
@@ -4,6 +4,7 @@
 
 package moriyashiine.enchancement.mixin.vanillachanges.singlelevelmode;
 
+import moriyashiine.enchancement.common.ModConfig;
 import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ItemMixin {
 	@Inject(method = "getRarity", at = @At("RETURN"), cancellable = true)
 	private void enchancement$singleLevelMode(ItemStack stack, CallbackInfoReturnable<Rarity> cir) {
-		if (stack.hasEnchantments() && !EnchancementUtil.hasWeakEnchantments(stack)) {
+		if (ModConfig.singleLevelMode && stack.hasEnchantments() && !EnchancementUtil.hasWeakEnchantments(stack)) {
 			cir.setReturnValue(Rarity.values()[Math.min(cir.getReturnValue().ordinal() + 1, Rarity.values().length - 1)]);
 		}
 	}
