62edf47f5a917794f6e1b4b8482f4518233d7920
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-04 13:43:03 -0500

    loyalty tridents no longer drop when the owner is dead, and return after respawn

--- src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/TridentEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/TridentEntityMixin.java
@@ -8,14 +8,33 @@ import moriyashiine.enchancement.common.ModConfig;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TridentEntity.class)
 public abstract class TridentEntityMixin extends PersistentProjectileEntity {
+	@Shadow
+	public abstract boolean isOwnerAlive();
+
 	protected TridentEntityMixin(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@ModifyVariable(method = "tick", at = @At("STORE"))
+	private int enchancement$rebalanceEquipment(int value) {
+		if (value > 0 && !isOwnerAlive()) {
+			if (getVelocity().length() > 0) {
+				setVelocity(getVelocity().multiply(0.9));
+			} else {
+				setVelocity(Vec3d.ZERO);
+			}
+			return 0;
+		}
+		return value;
 	}
 
 	@ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/TridentEntity;isNoClip()Z"))
---
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/client/TridentEntityRendererMixin.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) MoriyaShiine. All Rights Reserved.
+ */
+package moriyashiine.enchancement.mixin.config.rebalanceequipment.client;
+
+import moriyashiine.enchancement.mixin.util.accessor.PersistentProjectileEntityAccessor;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemEntityRenderer;
+import net.minecraft.client.render.entity.TridentEntityRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TridentEntityRenderer.class)
+public class TridentEntityRendererMixin {
+	@Unique
+	private static final MinecraftClient client = MinecraftClient.getInstance();
+	@Unique
+	private final Random random = Random.create();
+
+	@Inject(method = "render(Lnet/minecraft/entity/projectile/TridentEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
+	private void enchancement$rebalanceEquipment(TridentEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+		if (entity.getDataTracker().get(TridentEntity.LOYALTY) > 0 && !entity.isOwnerAlive()) {
+			matrices.push();
+			ItemStack itemStack = ((PersistentProjectileEntityAccessor) entity).enchancement$asItemStack();
+			random.setSeed(ItemEntityRenderer.getSeed(itemStack));
+			BakedModel model = client.getItemRenderer().getModel(itemStack, entity.getWorld(), null, entity.getId());
+			matrices.translate(0.0F, MathHelper.sin((entity.age + tickDelta) / 10.0F) * 0.1F + 0.1F + 0.25F * model.getTransformation().getTransformation(ModelTransformationMode.GROUND).scale.y(), 0.0F);
+			matrices.multiply(RotationAxis.POSITIVE_Y.rotation((entity.age + tickDelta) / 20F));
+			ItemEntityRenderer.renderStack(client.getItemRenderer(), matrices, vertexConsumers, light, itemStack, model, model.hasDepth(), random);
+			matrices.pop();
+			ci.cancel();
+		}
+	}
+}
