5e9dffe0f36e224b1778eddf5f8f27a47bd55c01
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-05 18:15:23 -0500

    fix grindstone not removing enchantments

--- src/main/java/moriyashiine/enchancement/mixin/config/enchantmentlimit/EnchantmentHelperMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/enchantmentlimit/EnchantmentHelperMixin.java
@@ -4,6 +4,8 @@
 package moriyashiine.enchancement.mixin.config.enchantmentlimit;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import moriyashiine.enchancement.common.ModConfig;
 import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
@@ -14,33 +16,34 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
-	@Inject(method = "apply", at = @At("HEAD"), cancellable = true)
-	private static void enchancement$enchantmentLimit(ItemStack stack, Consumer<ItemEnchantmentsComponent.Builder> applier, CallbackInfoReturnable<ItemEnchantmentsComponent> cir) {
-		if (EnchancementUtil.limitCheck(false, EnchancementUtil.getNonDefaultEnchantmentsSize(stack, stack.getEnchantments().getSize()) >= ModConfig.enchantmentLimit)) {
-			cir.setReturnValue(stack.getEnchantments());
+	@Unique
+	private static ItemEnchantmentsComponent removeUntilReady(ItemStack stack, ItemEnchantmentsComponent itemEnchantmentsComponent) {
+		List<RegistryEntry<Enchantment>> enchantments = new ArrayList<>(itemEnchantmentsComponent.getEnchantments());
+		while (EnchancementUtil.limitCheck(false, EnchancementUtil.getNonDefaultEnchantmentsSize(stack, enchantments.size()) > ModConfig.enchantmentLimit)) {
+			enchantments.removeFirst();
 		}
+		ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(itemEnchantmentsComponent);
+		builder.remove(enchantment -> !enchantments.contains(enchantment));
+		return builder.build();
+	}
+
+	@WrapOperation(method = "apply", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/ItemEnchantmentsComponent$Builder;build()Lnet/minecraft/component/type/ItemEnchantmentsComponent;"))
+	private static ItemEnchantmentsComponent enchancement$enchantmentLimit(ItemEnchantmentsComponent.Builder instance, Operation<ItemEnchantmentsComponent> original, ItemStack stack) {
+		return removeUntilReady(stack, original.call(instance));
 	}
 
 	@ModifyVariable(method = "set", at = @At("HEAD"), argsOnly = true)
 	private static ItemEnchantmentsComponent enchancement$enchantmentLimit(ItemEnchantmentsComponent value, ItemStack stack) {
-		List<RegistryEntry<Enchantment>> enchantments = new ArrayList<>(value.getEnchantments());
-		while (EnchancementUtil.limitCheck(false, EnchancementUtil.getNonDefaultEnchantmentsSize(stack, enchantments.size()) > ModConfig.enchantmentLimit)) {
-			enchantments.removeFirst();
-		}
-		ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(value);
-		builder.remove(enchantment -> !enchantments.contains(enchantment));
-		return builder.build();
+		return removeUntilReady(stack, value);
 	}
 
 	@ModifyReturnValue(method = "generateEnchantments", at = @At(value = "RETURN", ordinal = 1))
