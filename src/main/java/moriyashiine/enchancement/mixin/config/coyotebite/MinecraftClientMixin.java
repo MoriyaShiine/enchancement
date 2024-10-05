e2dbcefd0c5d29d5b7deffbc11cecbb30a503222
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-05 00:05:03 -0500

    fix coyote bite applying before other mods can cancel attack

--- src/main/java/moriyashiine/enchancement/mixin/config/coyotebite/MinecraftClientMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/coyotebite/MinecraftClientMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftClient.class)
+@Mixin(value = MinecraftClient.class, priority = 1001)
 public class MinecraftClientMixin {
 	@Shadow
 	@Nullable
