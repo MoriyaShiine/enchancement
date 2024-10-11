9a713655f8d2317a35c1bfbd1a977c27ffb7d986
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-11 09:01:38 -0500

    projectiles are now stronger against armor and deal normal damage without it

--- src/main/java/moriyashiine/enchancement/mixin/config/rebalanceprojectiles/PersistentProjectileEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceprojectiles/PersistentProjectileEntityMixin.java
@@ -7,24 +7,25 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import moriyashiine.enchancement.common.ModConfig;
-import moriyashiine.enchancement.common.event.SyncVelocitiesEvent;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.projectile.ProjectileUtil;
-import net.minecraft.util.math.Box;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(ProjectileUtil.class)
+@Mixin(PersistentProjectileEntity.class)
 public class PersistentProjectileEntityMixin {
-	@WrapOperation(method = "getEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;F)Lnet/minecraft/util/hit/EntityHitResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Box;expand(D)Lnet/minecraft/util/math/Box;"))
-	private static Box enchancement$rebalanceProjectiles(Box instance, double value, Operation<Box> original, @Local(ordinal = 2) Entity entity) {
+	@WrapOperation(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/PersistentProjectileEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;", ordinal = 0))
+	private Vec3d enchancement$rebalanceProjectiles(PersistentProjectileEntity instance, Operation<Vec3d> original, @Local Entity entity) {
+		Vec3d velocity = original.call(instance);
 		if (ModConfig.rebalanceProjectiles) {
-			Vec3d velocity = SyncVelocitiesEvent.VELOCITIES.getOrDefault(entity.getUuid(), entity.getVelocity());
-			Vec3d adjustedVelocity = new Vec3d(velocity.getX(), Math.max(0, velocity.getY()), velocity.getZ());
-			value *= MathHelper.lerp(Math.min(1, adjustedVelocity.length() * 2), 1, 5);
+			velocity = velocity.multiply(2 / 3F);
+			if (entity instanceof LivingEntity living) {
+				velocity = velocity.multiply(MathHelper.lerp(Math.min(20, living.getArmor()) / 20F, 1, 1.5F));
+			}
 		}
-		return original.call(instance, value);
+		return velocity;
 	}
 }
