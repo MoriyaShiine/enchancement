2281d1c66a50f2f5272cd9086655b87762386ca2
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-03-10 10:56:02 -0500

    tridents thrown by players no longer despawn

--- src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/TridentEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/TridentEntityMixin.java
@@ -6,22 +6,52 @@ package moriyashiine.enchancement.mixin.config.rebalanceequipment;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import moriyashiine.enchancement.common.ModConfig;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TridentEntity.class)
 public abstract class TridentEntityMixin extends PersistentProjectileEntity {
+	@Unique
+	private boolean ownedByPlayer = false;
+
 	@Shadow
 	public abstract boolean isOwnerAlive();
 
 	protected TridentEntityMixin(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	private void enchancement$rebalanceEquipment(CallbackInfo ci) {
+		if (age % 20 == 0 && !ownedByPlayer && getOwner() instanceof PlayerEntity) {
+			ownedByPlayer = true;
+		}
+	}
+
+	@ModifyVariable(method = "age", at = @At("STORE"))
+	private int enchancement$rebalanceEquipmentPreventDespawn(int value) {
+		return ownedByPlayer ? 1 : value;
+	}
+
+	@Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+	private void enchancement$rebalanceEquipmentRead(NbtCompound nbt, CallbackInfo ci) {
+		ownedByPlayer = nbt.getBoolean("OwnedByPlayer");
+	}
+
+	@Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+	private void enchancement$rebalanceEquipmentWrite(NbtCompound nbt, CallbackInfo ci) {
+		nbt.putBoolean("OwnedByPlayer", ownedByPlayer);
 	}
 
 	@ModifyVariable(method = "tick", at = @At("STORE"))
