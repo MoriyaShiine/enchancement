cf0d7716c65ab9dcc70f4f29bfcee40293ace4be
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-05-10 09:28:38 -0500

    fix veil decreasing detection range of non-hostile entities

--- src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/modifydetectionrange/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/modifydetectionrange/LivingEntityMixin.java
@@ -9,6 +9,7 @@ import moriyashiine.enchancement.common.tag.ModEntityTypeTags;
 import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.Monster;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -16,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class LivingEntityMixin {
 	@ModifyReturnValue(method = "getAttackDistanceScalingFactor", at = @At("RETURN"))
 	private double enchancement$modifyDetectionRange(double original, Entity entity) {
-		if (entity == null || !entity.getType().isIn(ModEntityTypeTags.VEIL_IMMUNE)) {
+		if (entity == null || (entity instanceof Monster && !entity.getType().isIn(ModEntityTypeTags.VEIL_IMMUNE))) {
 			return EnchancementUtil.getValue(ModEnchantmentEffectComponentTypes.MODIFY_DETECTION_RANGE, (LivingEntity) (Object) this, (float) original);
 		}
 		return original;
