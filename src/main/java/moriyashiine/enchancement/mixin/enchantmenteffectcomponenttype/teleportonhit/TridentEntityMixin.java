ab115d7acaee8dd010dc3d94084ae00cedb247d4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-05-19 19:54:56 -0500

    warp projectiles now return to their owner if they get hurt

--- src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/teleportonhit/TridentEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/teleportonhit/TridentEntityMixin.java
@@ -3,7 +3,9 @@
  */
 package moriyashiine.enchancement.mixin.enchantmenteffectcomponenttype.teleportonhit;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import moriyashiine.enchancement.common.component.entity.TeleportOnHitComponent;
+import moriyashiine.enchancement.common.init.ModEntityComponents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
@@ -24,5 +26,16 @@ public abstract class TridentEntityMixin extends PersistentProjectileEntity {
 	@Inject(method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;)V", at = @At("TAIL"))
 	private void enchancement$teleportOnHit(World world, LivingEntity owner, ItemStack stack, CallbackInfo ci) {
 		TeleportOnHitComponent.maybeSet(owner, stack, this);
+	}
+
+	@ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/TridentEntity;isNoClip()Z"))
+	private boolean enchancement$rebalanceEquipment(boolean value) {
+		TeleportOnHitComponent teleportOnHitComponent = ModEntityComponents.TELEPORT_ON_HIT.get(this);
+		if (teleportOnHitComponent.teleportsOnBlockHit() || teleportOnHitComponent.teleportsOnEntityHit()) {
+			if (getOwner() instanceof LivingEntity living && living.hurtTime != 0) {
+				return true;
+			}
+		}
+		return value;
 	}
 }
