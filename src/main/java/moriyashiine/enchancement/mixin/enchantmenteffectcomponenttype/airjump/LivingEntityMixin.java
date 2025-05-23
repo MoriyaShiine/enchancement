743f195d658e8e6c53589bdda1c2063113ff5864
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-05-23 12:06:56 -0500

    fix gale doubling fall distance after jumping twice

--- src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/airjump/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/airjump/LivingEntityMixin.java
@@ -3,25 +3,21 @@
  */
 package moriyashiine.enchancement.mixin.enchantmenteffectcomponenttype.airjump;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import moriyashiine.enchancement.common.component.entity.AirJumpComponent;
 import moriyashiine.enchancement.common.init.ModEntityComponents;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-	@WrapOperation(method = "computeFallDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getAttributeValue(Lnet/minecraft/registry/entry/RegistryEntry;)D", ordinal = 0))
-	private double enchancement$airJump(LivingEntity instance, RegistryEntry<EntityAttribute> attribute, Operation<Double> original) {
-		double value = original.call(instance, attribute);
+	@ModifyReturnValue(method = "getUnsafeFallDistance", at = @At("RETURN"))
+	private double enchancement$airJump(double original) {
 		AirJumpComponent airJumpComponent = ModEntityComponents.AIR_JUMP.getNullable(this);
 		if (airJumpComponent != null && airJumpComponent.hasAirJump()) {
-			return value + airJumpComponent.getMaxJumps() - airJumpComponent.getJumpsLeft();
+			original -= airJumpComponent.getMaxJumps() - airJumpComponent.getJumpsLeft();
 		}
-		return value;
+		return original;
 	}
 }
