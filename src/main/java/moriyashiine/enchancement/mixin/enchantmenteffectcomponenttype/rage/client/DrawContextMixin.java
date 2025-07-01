2cd028cad1f7f9003361c6fa6676371068616473
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-07-01 14:32:56 -0500

    update to 1.21.7

--- src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/rage/client/DrawContextMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/rage/client/DrawContextMixin.java
@@ -7,7 +7,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import moriyashiine.enchancement.client.render.item.ItemRenderStateAddition;
 import moriyashiine.enchancement.common.enchantment.effect.RageEffect;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.client.render.item.KeyedItemRenderState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(DrawContext.class)
 public class DrawContextMixin {
 	@Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;III)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/item/ItemModelManager;clearAndUpdate(Lnet/minecraft/client/render/item/ItemRenderState;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemDisplayContext;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;I)V"))
-	private void enchancement$rage(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, CallbackInfo ci, @Local ItemRenderState itemRenderState) {
+	private void enchancement$rage(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, CallbackInfo ci, @Local KeyedItemRenderState itemRenderState) {
 		int color = RageEffect.getColor(entity, stack);
 		if (color != -1) {
 			((ItemRenderStateAddition) itemRenderState).enchancement$setRageColor(color);
