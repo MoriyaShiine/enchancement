c16a6feefffece1b4a650d6ac9c66afdacd83187
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-08-17 15:53:17 -0500

    cleaner armor rebalance code

--- src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/ArmorMaterialMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceequipment/ArmorMaterialMixin.java
@@ -3,43 +3,36 @@
  */
 package moriyashiine.enchancement.mixin.config.rebalanceequipment;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import moriyashiine.enchancement.common.ModConfig;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.item.equipment.ArmorMaterial;
-import net.minecraft.item.equipment.ArmorMaterials;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.item.equipment.EquipmentAsset;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
+import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.registry.RegistryKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.Map;
 
 @Mixin(ArmorMaterial.class)
 public class ArmorMaterialMixin {
-	@WrapOperation(method = "createAttributeModifiers", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/AttributeModifiersComponent$Builder;add(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/entity/attribute/EntityAttributeModifier;Lnet/minecraft/component/type/AttributeModifierSlot;)Lnet/minecraft/component/type/AttributeModifiersComponent$Builder;", ordinal = 0))
-	private AttributeModifiersComponent.Builder enchancement$rebalanceEquipment(AttributeModifiersComponent.Builder instance, RegistryEntry<EntityAttribute> attribute, EntityAttributeModifier modifier, AttributeModifierSlot slot, Operation<AttributeModifiersComponent.Builder> original) {
-		if (ModConfig.rebalanceEquipment) {
-			int change = 0;
-			if ((Object) this == ArmorMaterials.IRON && slot == AttributeModifierSlot.FEET) {
-				change++;
-			}
-			if ((Object) this == ArmorMaterials.GOLD && slot == AttributeModifierSlot.CHEST) {
-				change--;
-			}
-			if (change != 0) {
-				modifier = new EntityAttributeModifier(modifier.id(), modifier.value() + change, modifier.operation());
-			}
+	@ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
+	private static Map<EquipmentType, Integer> enchancement$rebalanceEquipment(Map<EquipmentType, Integer> defense, @Local(argsOnly = true) RegistryKey<EquipmentAsset> assetId) {
+		if (assetId.equals(EquipmentAssetKeys.IRON)) {
+			defense.put(EquipmentType.BOOTS, defense.get(EquipmentType.BOOTS) + 1);
 		}
-		return original.call(instance, attribute, modifier, slot);
+		if (assetId.equals(EquipmentAssetKeys.GOLD)) {
+			defense.put(EquipmentType.CHESTPLATE, defense.get(EquipmentType.CHESTPLATE) - 1);
+		}
+		return defense;
 	}
 
-	@WrapOperation(method = "createAttributeModifiers", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/AttributeModifiersComponent$Builder;add(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/entity/attribute/EntityAttributeModifier;Lnet/minecraft/component/type/AttributeModifierSlot;)Lnet/minecraft/component/type/AttributeModifiersComponent$Builder;", ordinal = 1))
-	private AttributeModifiersComponent.Builder enchancement$rebalanceEquipmentToughness(AttributeModifiersComponent.Builder instance, RegistryEntry<EntityAttribute> attribute, EntityAttributeModifier modifier, AttributeModifierSlot slot, Operation<AttributeModifiersComponent.Builder> original) {
-		if (ModConfig.rebalanceEquipment && (Object) this == ArmorMaterials.IRON) {
-			modifier = new EntityAttributeModifier(modifier.id(), modifier.value() + 1, modifier.operation());
+	@ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+	private static float enchancement$rebalanceEquipment(float toughness, @Local(argsOnly = true) RegistryKey<EquipmentAsset> assetId) {
+		if (assetId.equals(EquipmentAssetKeys.IRON)) {
+			return toughness + 1;
 		}
-		return original.call(instance, attribute, modifier, slot);
+		return toughness;
 	}
 }
