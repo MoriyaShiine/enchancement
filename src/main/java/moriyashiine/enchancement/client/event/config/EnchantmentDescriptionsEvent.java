16a4a28037ccf5a164313ed11cf6ac7e44e534ad
EightSidedSquare <contact@eightsidedsquare.com>
2025-09-16 23:41:24 -0400

    Upgrade Stored Enchantments Tooltip Component

--- src/main/java/moriyashiine/enchancement/client/event/config/EnchantmentDescriptionsEvent.java
+++ src/main/java/moriyashiine/enchancement/client/event/config/EnchantmentDescriptionsEvent.java
@@ -9,12 +9,16 @@ import moriyashiine.enchancement.common.ModConfig;
 import moriyashiine.enchancement.common.util.EnchancementUtil;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipData;
 import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
@@ -30,13 +34,10 @@ public class EnchantmentDescriptionsEvent {
 			if (enableDescriptions()) {
 				EnchantmentHelper.getEnchantments(stack).getEnchantments().forEach(enchantment -> {
 					for (int i = 0; i < lines.size(); i++) {
-						String translationKey = EnchancementUtil.getTranslationKey(enchantment);
-						if (lines.get(i).getContent() instanceof TranslatableTextContent text && text.getKey().equals(translationKey)) {
-							MutableText description = Text.translatable(translationKey + ".desc").formatted(Formatting.DARK_GRAY);
-							if (!description.getString().isEmpty()) {
-								lines.add(i + 1, Text.literal(" - ").formatted(Formatting.GRAY).append(description));
-								break;
-							}
+						Text description = getDescription(lines.get(i), enchantment, true);
+						if(description != null) {
+							lines.add(i + 1, description);
+							break;
 						}
 					}
 				});
@@ -54,7 +55,28 @@ public class EnchantmentDescriptionsEvent {
 		}
 	}
 
+	public static class ClearIconCache implements CommonLifecycleEvents.TagsLoaded {
+		@Override
+		public void onTagsLoaded(DynamicRegistryManager registries, boolean client) {
+			if (client) {
+				StoredEnchantmentsTooltipComponent.clearIconCache();
+			}
+		}
+	}
+
 	public static boolean enableDescriptions() {
 		return ModConfig.enchantmentDescriptions && !Enchancement.commonEnchantmentDescriptionsModLoaded;
+	}
+
+	@Nullable
+	public static Text getDescription(Text text, RegistryEntry<Enchantment> enchantment, boolean hyphen) {
+		String translationKey = EnchancementUtil.getTranslationKey(enchantment);
+		if (text.getContent() instanceof TranslatableTextContent textContent && textContent.getKey().equals(translationKey)) {
+			MutableText description = Text.translatable(translationKey + ".desc").formatted(Formatting.DARK_GRAY);
+			if (!description.getString().isEmpty()) {
+				return hyphen ? Text.literal(" - ").formatted(Formatting.GRAY).append(description) : description;
+			}
+		}
+		return null;
 	}
 }
