6e21858e3f2663a77fb7b8e12345ad019775c745
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-10-11 13:00:36 -0500

    fix crash when rendering mannequins

--- src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/slam/client/PlayerEntityRendererMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/enchantmenteffectcomponenttype/slam/client/PlayerEntityRendererMixin.java
@@ -3,11 +3,13 @@
  */
 package moriyashiine.enchancement.mixin.enchantmenteffectcomponenttype.slam.client;
 
+import moriyashiine.enchancement.common.component.entity.SlamComponent;
 import moriyashiine.enchancement.common.init.ModEntityComponents;
 import net.minecraft.client.network.ClientPlayerLikeEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.entity.PlayerLikeEntity;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +19,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerEntityRendererMixin<AvatarlikeEntity extends PlayerLikeEntity & ClientPlayerLikeEntity> {
 	@Inject(method = "updateRenderState(Lnet/minecraft/entity/PlayerLikeEntity;Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;F)V", at = @At("TAIL"))
 	private void enchancement$slam(AvatarlikeEntity entity, PlayerEntityRenderState state, float tickProgress, CallbackInfo ci) {
-		if (ModEntityComponents.SLAM.get(entity).isSlamming()) {
+		@Nullable SlamComponent slamComponent = ModEntityComponents.SLAM.getNullable(entity);
+		if (slamComponent != null && slamComponent.isSlamming()) {
 			state.isInSneakingPose = state.sneaking = true;
 		}
 	}
