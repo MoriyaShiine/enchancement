3af1bea4e3e5b3ca2e3831a2f37940b2b8788219
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-05-01 20:40:30 -0500

    fix push components resetting when losing durability

--- src/main/java/moriyashiine/enchancement/common/event/enchantmenteffectcomponenttype/EquipmentResetEvent.java
+++ src/main/java/moriyashiine/enchancement/common/event/enchantmenteffectcomponenttype/EquipmentResetEvent.java
@@ -6,6 +6,7 @@ package moriyashiine.enchancement.common.event.enchantmenteffectcomponenttype;
 
 import moriyashiine.enchancement.common.component.entity.enchantmenteffectcomponenttype.util.PushComponent;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -15,10 +16,12 @@ import org.ladysnake.cca.api.v3.component.ComponentKey;
 public class EquipmentResetEvent implements ServerEntityEvents.EquipmentChange {
 	@Override
 	public void onChange(LivingEntity livingEntity, EquipmentSlot equipmentSlot, ItemStack previousStack, ItemStack currentStack) {
-		if (equipmentSlot.getType() != EquipmentSlot.Type.HAND) {
-			for (ComponentKey<?> key : livingEntity.asComponentProvider().getComponentContainer().keys()) {
-				if (livingEntity.getComponent(key) instanceof PushComponent pushComponent && EnchantmentHelper.has(currentStack, pushComponent.getEffectType())) {
-					pushComponent.resetNextTick();
+		if (!ItemStack.matchesIgnoringComponents(previousStack, currentStack, DataComponentType::ignoreSwapAnimation)) {
+			if (equipmentSlot.getType() != EquipmentSlot.Type.HAND) {
+				for (ComponentKey<?> key : livingEntity.asComponentProvider().getComponentContainer().keys()) {
+					if (livingEntity.getComponent(key) instanceof PushComponent pushComponent && EnchantmentHelper.has(currentStack, pushComponent.getEffectType())) {
+						pushComponent.resetNextTick();
+					}
 				}
 			}
 		}
