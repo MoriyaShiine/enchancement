b0d850973e1de3237d59a8c37383d06f420002d4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-05-03 10:58:01 -0500

    fix riptide slowdown with amphibious

--- src/main/java/moriyashiine/enchancement/mixin/config/rebalanceenchantments/LivingEntityMixin.java
+++ src/main/java/moriyashiine/enchancement/mixin/config/rebalanceenchantments/LivingEntityMixin.java
@@ -5,18 +5,43 @@
 package moriyashiine.enchancement.mixin.config.rebalanceenchantments;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import moriyashiine.enchancement.common.ModConfig;
 import moriyashiine.enchancement.common.init.ModEntityComponents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin {
+	@Shadow
+	public abstract boolean isAutoSpinAttack();
+
+	@Shadow
+	public abstract boolean isUsingItem();
+
+	@Shadow
+	public abstract ItemStack getActiveItem();
+
 	@ModifyExpressionValue(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/DamageSource;is(Lnet/minecraft/tags/TagKey;)Z", ordinal = 4))
 	private boolean enchancement$rebalanceEnchantments(boolean original, @Local(argsOnly = true) DamageSource source) {
 		return original || (source.is(DamageTypes.ON_FIRE) && ModEntityComponents.IGNITE_KNOCKBACK.get(this).isIgnited());
+	}
+
+	@SuppressWarnings("ConstantValue")
+	@ModifyExpressionValue(method = "travelInWater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/core/Holder;)D"))
+	private double enchancement$rebalanceEnchantments(double original) {
+		if (ModConfig.rebalanceEnchantments) {
+			if (isAutoSpinAttack() || (isUsingItem() && EnchantmentHelper.getTridentSpinAttackStrength(getActiveItem(), (LivingEntity) (Object) this) > 0)) {
+				return 0;
+			}
+		}
+		return original;
 	}
 }
