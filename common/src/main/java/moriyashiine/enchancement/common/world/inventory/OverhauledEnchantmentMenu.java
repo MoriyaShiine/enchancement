222e8ea21519dcead95ab3ec11149ef8dfe49778
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-09-13 18:26:53 -0500

    chiseled bookshelves now count the total collected books for enchantment power instead of per individual block

--- common/src/main/java/moriyashiine/enchancement/common/world/inventory/OverhauledEnchantmentMenu.java
+++ common/src/main/java/moriyashiine/enchancement/common/world/inventory/OverhauledEnchantmentMenu.java
@@ -287,22 +287,22 @@ public class OverhauledEnchantmentMenu extends AbstractContainerMenu {
 	private void collectBookshelves(ServerPlayer player) {
 		access.execute((level, pos) -> {
 			chiseledEnchantments.clear();
-			bookshelfCount = 0;
+			float power = 0;
 			for (BlockPos offset : EnchantingTableBlock.BOOKSHELF_OFFSETS) {
 				if (EnchantingTableBlock.isValidBookShelf(level, pos, offset)) {
 					if (level.getBlockEntity(pos.offset(offset)) instanceof ChiseledBookShelfBlockEntity chiseledBookshelfBlockEntity) {
-						bookshelfCount += chiseledBookshelfBlockEntity.count() / 3;
+						power += chiseledBookshelfBlockEntity.count() / 3F;
 						if (EnchancementConfig.overhaulEnchanting == OverhaulMode.CHISELED && !player.hasInfiniteMaterials()) {
 							for (ItemStack stack : chiseledBookshelfBlockEntity) {
 								chiseledEnchantments.addAll(EnchantmentHelper.getEnchantmentsForCrafting(stack).keySet());
 							}
 						}
 					} else {
-						bookshelfCount++;
+						power++;
 					}
 				}
 			}
-			bookshelfCount = Math.min(15, bookshelfCount);
+			bookshelfCount = Mth.clamp(Mth.floor(power), 0, 15);
 			if (EnchancementConfig.overhaulEnchanting == OverhaulMode.CHISELED && player.hasInfiniteMaterials()) {
 				Registry<Enchantment> enchantments = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
 				enchantments.forEach(enchantment -> chiseledEnchantments.add(enchantments.wrapAsHolder(enchantment)));
