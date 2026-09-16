e1779d49890479a5462195d4c10625229837f8e3
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-09-16 08:19:48 -0500

    refactor registration
    (cherry picked from commit 70f0c2ad51bb4e1ff4dfbb026426b58f1fc229d9)

--- common/src/main/java/moriyashiine/enchancement/common/init/EnchancementParticleTypes.java
+++ common/src/main/java/moriyashiine/enchancement/common/init/EnchancementParticleTypes.java
@@ -12,9 +12,9 @@ public class EnchancementParticleTypes {
 	public static final SimpleParticleType BRIMSTONE_BUBBLE = registerParticleType("brimstone_bubble", FabricParticleTypes.simple());
 	public static final SimpleParticleType CHISELED_ENCHANT = registerParticleType("chiseled_enchant", FabricParticleTypes.simple());
 	public static final SimpleParticleType CRITICAL_TIPPER = registerParticleType("critical_tipper", FabricParticleTypes.simple());
-	public static final ParticleType<HoneyBubbleParticleOption> HONEY_BUBBLE = registerParticleType("honey_bubble", FabricParticleTypes.complex(HoneyBubbleParticleOption.CODEC, HoneyBubbleParticleOption.PACKET_CODEC));
+	public static final ParticleType<HoneyBubbleParticleOption> HONEY_BUBBLE = registerParticleType("honey_bubble", FabricParticleTypes.complex(HoneyBubbleParticleOption.CODEC, HoneyBubbleParticleOption.STREAM_CODEC));
 	public static final SimpleParticleType SHORT_SMALL_FLAME = registerParticleType("short_small_flame", FabricParticleTypes.simple());
-	public static final ParticleType<SparkParticleOption> SPARK = registerParticleType("spark", FabricParticleTypes.complex(SparkParticleOption.CODEC, SparkParticleOption.PACKET_CODEC));
+	public static final ParticleType<SparkParticleOption> SPARK = registerParticleType("spark", FabricParticleTypes.complex(SparkParticleOption.CODEC, SparkParticleOption.STREAM_CODEC));
 	public static final SimpleParticleType VELOCITY_LINE = registerParticleType("velocity_line", FabricParticleTypes.simple());
 
 	public static void init() {
